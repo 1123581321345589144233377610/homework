cf8c7aa6f6acca37e1530fe8758333e8ed33c046
Dima M. Shirokov <D.Shirokov05@yandex.ru>
2024-07-17 17:28:44 +0300

    done

---
+++ homework-5/5-1/func.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int sub(int a, int b) {
+	return a - b;
+}
+
+int mult(int a, int b) {
+	return a * b;
+}
+
+int division(int a, int b) {
+	return a / b;
+}
+
+int power(int a, int b) {
+	return pow(a, b);
+}
---
+++ homework-5/5-1/func.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int add(int, int);
+int sub(int, int);
+int mult(int, int);
+int division(int, int);
+int power(int, int);
