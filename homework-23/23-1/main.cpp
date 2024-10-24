66b4379d36df051a0b6a90763e5957401561c611
DimaShirokov <D.Shirokov05@yandex.ru>
2024-10-24 17:20:01 +0300

    done

---
+++ homework-23/23-1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+
+void print_letter_frequency(const std::string &str) {
+	std::unordered_map<char, unsigned int> umap{};
+	std::vector<std::pair<char, unsigned int>> vector{};
+	std::for_each(str.begin(), str.end(), [&umap](const char &ch){ ++umap[ch]; });
+	std::for_each(umap.begin(), umap.end(), [&vector](const auto& item) { vector.push_back(item); });
+	std::sort(vector.begin(), vector.end(), [&vector](const auto& item_first, const auto& item_second) { return item_first.second > item_second.second; });
+	std::for_each(vector.begin(), vector.end(), [&vector](const auto &item) { std::cout << item.first << ": " << item.second << std::endl; });
+}
+
+int main(int argc, char* argv[]) {
+	print_letter_frequency("Hello world!!");
+	return EXIT_SUCCESS;
+}
