142b8aa1026451bebeb0be1cccdea6298ee43f1c
Dima M. Shirokov <D.Shirokov05@yandex.ru>
2024-10-31 22:19:37 +0300

    done

---
+++ homework-25/25-3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+template <class Type>
+class unique_ptr {
+private:
+	Type* ptr;
+public:
+	unique_ptr() : ptr{nullptr} {}
+	unique_ptr(Type* ptr_) : ptr{ptr_} {}
+	unique_ptr(const unique_ptr<Type>& other) = delete;
+	Type operator*() {
+		return *ptr;
+	}
+	unique_ptr<Type>& operator=(const unique_ptr<Type>& other) = delete;
+	Type* release() {
+		Type* new_ptr = ptr;
+		ptr = nullptr;
+		return new_ptr;
+	}
+	~unique_ptr() {
+		delete ptr;
+		ptr = nullptr;
+	}
+};
+
+int main(int argc, char* argv[]) {
+	unique_ptr<int> ptr(new int(10));
+	std::cout << *(ptr) << std::endl;
+	int* new_ptr = ptr.release();
+	std::cout << *(new_ptr) << std::endl;
+	return EXIT_SUCCESS;
+}
