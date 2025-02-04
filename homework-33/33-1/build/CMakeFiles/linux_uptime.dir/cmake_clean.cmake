fa5ea8ae2b74b972c49aefaf8a19cc645e9f6df5
Dima M Shirokov <D.Shirokov05@yandex.ru>
2025-02-04 21:58:02 +0100

    done first task

---
+++ homework-33/33-1/build/CMakeFiles/linux_uptime.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linux_uptime.dir/main.c.o"
+  "CMakeFiles/linux_uptime.dir/main.c.o.d"
+  "linux_uptime"
+  "linux_uptime.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linux_uptime.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework-33/33-1/build/CMakeFiles/linux_uptime.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for linux_uptime.
+# This may be replaced when dependencies are built.
