43ced059fd99dbcb3a42de9dcb519a5f405afd24
Dima M Shirokov <D.Shirokov05@yandex.ru>
2025-03-03 22:41:07 +0300

    done

---
+++ homework-36/36-1/main.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void Print(const std::string& message) = 0;
+};
+
+class PrintMessageInConsole : public LogCommand{
+public:
+    void Print(const std::string& message) override{
+        std::cout << message << std::endl;
+    }
+};
+
+class PrintMessageInFile : public LogCommand{
+private:
+    std::ofstream output;
+public:
+    PrintMessageInFile(const std::string& fileName){
+        output = std::ofstream(fileName);
+    }
+    void Print(const std::string& message) override{
+        output << message << std::endl;
+    }
+};
+
+void Print(LogCommand* command, const std::string& message){
+    command->Print(message);
+}
+
+int main(int argc, char* argv[]){
+    LogCommand* first = new PrintMessageInConsole();
+    LogCommand* second = new PrintMessageInFile("output.txt");
+    Print(first, "Hello world!");
+    Print(second, "Hello world!");
+    return EXIT_SUCCESS;
+}
